3c560ec78ef9c834684173c12e9602cdb6f9d5c1
Zulpikar <zulpikaruygur@cybertekschool.com>
2022-05-14 18:43:30 +0300

    added Day3 file

---
+++ src/test/java/day3/Day3.java
@@ -0,0 +1,35 @@
+package day3;
+
+public class Day3 {
+
+    /*
+    lombok
+    how to add my name into lines
+    using different examples for pojo
+    postman post, put , patch,  delete request
+     */
+
+
+    /*
+    1. using zipcode API, find 20113 zipcode in US. put the result into a map
+    2. do the above task again, put the result into a custom class.
+    3. using zipcode API, find fairfax information and put into a map
+    4. do the above task and put the result into a custom class
+    5. using hr api, location table, put all the country_id into a list
+    6. using hr api, departments table, put all the department link into a list
+    7. using hr jobs table, put the response into a custom class
+    8. use question 7 response, only put the 5th job to a custom class
+    9. using hr regions table, put the response into a list(using jsonpath method)
+    10. add a new spartan in below ways:
+        - postman manually
+        - using String
+        - using Map
+        - custom class
+
+    11. replace above spartan with a new spartan information
+    12. change only the phone of above spartan
+    13. delete above spartan
+     */
+
+
+}
